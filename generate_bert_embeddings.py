76be1883ce95d30a574c19b57c40a87c18b06af7
romainf28 <romain.fournier@student-cs.fr>
2023-04-05 15:12:36 +0200

    save ood datasets embeddings

--- generate_bert_embeddings.py
+++ generate_bert_embeddings.py
@@ -6,6 +6,7 @@ import pickle
 from custom_bert import CustomBert
 import sys
 from tqdm import tqdm
+import json
 
 
 def load_tokenizer_from_checkpoint(checkpoint):
@@ -23,23 +24,23 @@ def load_model_from_checkpoint(checkpoint):
         checkpoint)
 
 
-def save_in_ds_embeddings(model, tokenizer, in_ds_name='imdb', aggregate=True):
+def save_in_ds_embeddings(model, tokenizer, in_ds_name='imdb', ds_names={'imdb': 'imdb'}, aggregate=True):
     '''
     Save the embeddings of the fine tuned model on the in-distribution dataset
     '''
-    dataset = load_dataset(in_ds_name)
+    dataset = load_dataset(ds_names[in_ds_name])
     # create data loader
     data_loader = DataLoader(dataset=dataset['test'], batch_size=8, collate_fn=lambda x: [
         el['text'] for el in x])
     in_ds_embeddings = []
 
-    latent_bert = CustomBert(model).to('cuda')
+    custom_bert = CustomBert(model).to('cuda')
     for batch in tqdm(data_loader):
         # tokenize batch
         batch_encoded_input = tokenizer(
             batch, return_tensors='pt', truncation=True, padding=True)['input_ids'].to('cuda')
         # forward pass of the custom bert model
-        outputs = latent_bert.forward(batch_encoded_input, aggregate=aggregate)
+        outputs = custom_bert.forward(batch_encoded_input, aggregate=aggregate)
         in_ds_embeddings.append(outputs['embeddings'].cpu().detach())
 
     # save embeddings
@@ -47,7 +48,35 @@ def save_in_ds_embeddings(model, tokenizer, in_ds_name='imdb', aggregate=True):
         pickle.dump(in_ds_embeddings, f)
 
 
+def save_ood_embeddings(model, tokenizer, in_ds_name='imdb', ds_names={'imdb': 'imdb'}, aggregate=True):
+    batch_sizes = {}
+    collate_fns = {}
+
+    custom_bert = CustomBert(model).to('cuda')
+
+    for ds_name, ds in ds_names.items():
+        if ds_name != in_ds_name:
+            ood_dataset = load_dataset(*ds)
+            data_loader = DataLoader(dataset=ood_dataset['test'],
+                                     batch_size=batch_sizes[ds_name],
+                                     collate_fn=collate_fns[ds_name],
+                                     )
+            ood_embeddings = []
+            for batch in tqdm(data_loader):
+                tk_batch = tokenizer(batch, return_tensors="pt", truncation=True, padding=True)[
+                    'input_ids'].to('cuda')
+                outputs = custom_bert.forward(tk_batch, aggregated=False)
+                ood_embeddings.append(outputs['embeddings'].cpu().detach())
+
+    with open('.pickle_files/embeddings_ood_test_{}.pkl'.format(ds_name), 'wb') as f:
+        pickle.dump(ood_embeddings, f)
+
+
 if __name__ == '__main__':
+    with open('datasets_benchmark.json') as json_ds:
+        ds_names = json.load(json_ds)
+        json_ds.close()
+
     checkpoint = sys.argv[1]
     in_ds_name = sys.argv[2]
     aggregate = sys.argv[3]
@@ -56,4 +85,9 @@ if __name__ == '__main__':
     model = load_model_from_checkpoint(checkpoint)
 
     # Save in-distribution embeddings
-    save_in_ds_embeddings(model, tokenizer, in_ds_name, aggregate)
+    save_in_ds_embeddings(model, tokenizer, in_ds_name=in_ds_name,
+                          ds_names=ds_names, aggregate=aggregate)
+
+    # save ood embeddings
+    save_ood_embeddings(model, tokenizer, in_ds_name=in_ds_name,
+                        ds_names=ds_names, aggregate=aggregate)
