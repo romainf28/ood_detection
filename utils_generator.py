b1abe1f68ec9550fef728a99901ffa0a25931d42
ROY Gauthier <gauthier.roy@student-cs.fr>
2023-04-05 12:19:29 +0200

    translation model

--- utils_generator.py
+++ utils_generator.py
@@ -1,57 +1,53 @@
 import numpy as np
-from scipy.spatial.distance import cdist
-from typing import Optional
+from typing import Optional, Literal
 import torch
+from transformers import models, AutoModelForSeq2SeqLM, tokenization_utils_base
 
 
-def softmax(x: np.ndarray) -> np.ndarray:
+
+
+
+def renyi_divergence(p: torch.tensor, q: torch.tensor, alpha: float) -> torch.tensor:
     '''
-    Returns the softmax of X.
-    '''
-    exp_x = np.exp(x)
-    return np.diag(1/np.sum(exp_x, axis=-1)).dot(exp_x)
-
-
-def logSumExp(X: np.ndarray) -> np.ndarray:
-    '''
-    Returns the log of the sum of the exponential of X lines
-    '''
-    return np.log(np.sum(np.exp(X), axis=1))
-
-
-def renyi_divergence(p: np.ndarray, q: np.ndarray, alpha: float) -> np.ndarray:
-    '''
-    p and q are array representing the distribution. x is the parameter of renyi divergence.
+    p and q are tensors representing the distribution. alpha is the parameter of renyi divergence.
     Computes the Renyi divergence between p and q.
     '''
-    return (1/(1-alpha)) * np.log(np.sum((p**alpha)/(q**(alpha-1))))
+    return (1/(1-alpha)) * torch.log(torch.sum((p**alpha)/(q**(alpha-1)), dim=2))
 
 
-def FR_distance(p: np.ndarray, q: np.ndarray) -> np.ndarray:
+def FR_distance(p: torch.tensor, q: torch.tensor) -> torch.tensor:
     '''
-    p and q are array representing the distribution. x is the parameter of renyi divergence.
+    p and q are tensors representing the distribution.
     Computes the Fischer Rao distance between p and q.
     '''
-    return (2/np.pi) * np.arccos(np.sum((p*q)**0.5))
+    return (2/np.pi) * torch.arccos(torch.sum((p*q)**0.5, dim=2))
 
 
-def set_norm_logits(x: torch.tensor, model:torch.model, max_token: int, voc:torch.tensor) -> np.ndarray:
-    set_norm_logits = []
-    while len(set_norm_logits)<max_token :
-        if len(set_norm_logits) >0:
-            y = voc[set_norm_logits[-1].multinomial(num_samples=1, replacement=True)]
-            set_norm_logits.append(torch.nn.softmax(model(torch.cat((x,y),0))))
-        else:
-            set_norm_logits.append(torch.nn.softmax(model(x)))
+class GenerationModel :
+    def __init__(self, model_name:str):
+        self.model = AutoModelForSeq2SeqLM.from_pretrained(model_name)
+
+        self.softmax = torch.nn.Softmax(dim=2)
+
+        self.vocab_size = self.model.config.vocab_size
+
+    def generate_set(self, x:tokenization_utils_base.BatchEncoding):
+        outputs = self.model.generate(**x, return_dict_in_generate=True,output_scores=True, num_beams=1)
+        output_scores = torch.stack(list(outputs.scores), dim=0).permute(1,0,2)
+        return self.softmax(output_scores), outputs.sequences
+    
+    def anomaly_score(self,x:tokenization_utils_base.BatchEncoding, divergence:Literal["Renyi", "FR"], 
+                      alpha:Optional[float])->int:
+        set_proba, _ = self.generate_set(x)
         
-    return np.ndarray(set_norm_logits)
+        uniform_proba = torch.ones(set_proba.shape)/self.vocab_size
 
-def anomaly_score(x: torch.tensor, set_norm_logits:np.ndarray, divergence:str, alpha:Optional[float])->int:
-    cum_div = 0
-    u = np.ones(len(set_norm_logits[0]))/len(set_norm_logits[0])
-    for p in set_norm_logits:
-        if p == "renyi":
-            cum_div += renyi_divergence(p,u,alpha)
-        else:
-            cum_div += FR_distance(p,u)
-    return (1/len(set_norm_logits)) * cum_div
+        if divergence == "Renyi":
+            if not alpha:
+                raise AttributeError("When you use Renyi divergence, you must define an alpha")
+            div = torch.sum(renyi_divergence(set_proba, uniform_proba, alpha), dim=1)
+        elif divergence =="FR":
+            div = torch.sum(FR_distance(set_proba, uniform_proba), dim=1)
+        else: 
+            raise AttributeError("The divergence should be 'Renyi' or 'FR'")
+        return (1/set_proba.shape[1]) * div
